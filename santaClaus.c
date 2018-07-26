c6fe00565b8f0a3e5558c19cf4bac0ef3c2bfd34
Alana Schwendler <alanaschwendler27@gmail.com>
2018-07-26 18:18:05 -0300

    Beginning kruskal

--- santaClaus.c
+++ santaClaus.c
@@ -1,9 +1,49 @@
-#include <stdioh.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
+typedef struct graph {
+	int v;
+	int e;
+	int *x;
+	int *y;
+	int *distances;
+} Graph_t;
+
+/*
+ * Kruskal's algorithm
+ * @param *set set with solution
+ * @param *g graph with all informations
+ */
+void kruskal(int *set, Graph_t *g);
+
 int main() {
+	int *set = malloc(sizeof(int));
+	assert(set);
+   	Graph_t g = malloc(sizeof(Graph_t));
+	assert(g);
+
+	printf("Vortexes: ");
+	scanf("%d", g->v); //vortexes
+	printf("Edges: ");
+	scanf("%d", g->e); //edges
 	
+	g->x = malloc(sizeof(int) * g->v);
+	g->y = malloc(sizeof(int) * g->v);
+	d->distances = malloc(sizeof(int) * g->e);
+	
+	if((g->v == g->e) && (g->e == 0)) {
+		return;
+	} else {
+		kruskal(set, g);
+	}
+	free(set);
+	free(g);
 	return 0;
 }
 
+/* 
+ * Kruskal's algorithm
+ */
+void kruskal(int *set, int v, int e) {
+}
